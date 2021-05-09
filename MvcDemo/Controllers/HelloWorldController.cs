8c775d617f441e3f3061716bbb628ce8b078534b
Carlos Eduardo Ocrospoma Sarasi <ocrospoma.c.e@gmail.com>
2021-05-08 23:52:05 -0500

    Adding moviesController

--- MvcDemo/Controllers/HelloWorldController.cs
+++ MvcDemo/Controllers/HelloWorldController.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 
 namespace MvcDemo.Controllers
 {
@@ -16,9 +11,12 @@ namespace MvcDemo.Controllers
         }
 
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
---
+++ MvcDemo/Data/MvcDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcDemo.Models;
+
+namespace MvcDemo.Data
+{
+    public class MvcDemoContext : DbContext
+    {
+        public MvcDemoContext (DbContextOptions<MvcDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
